2b976ea254d9879f864c1c19ceb8dff9a19be974
Lilian VERLHAC <lilian.verlhac@epitech.eu>
2020-05-11 22:25:52 +0200

    [FIX] argument manager (allow float as convergence limit)

--- src/ArgumentManager.hs
+++ src/ArgumentManager.hs
@@ -4,10 +4,10 @@ module ArgumentManager
       getPath,
       lexer,
       parser,
-      Argument,
-      ColorLimit,
-      ConvergenceLimit,
-      Path
+      Argument (..),
+      ColorLimit (..),
+      ConvergenceLimit (..),
+      Path (..)
     ) where
 
 import Text.Read (readMaybe)
@@ -15,28 +15,36 @@ import Control.Exception (throw)
 
 import Exception (ICExceptions (BadArgument, SendHelp))
 
-type ColorLimit = Int
-type ConvergenceLimit = Int
-type Path = String
+newtype ColorLimit = ColorLimit Int deriving(Show)
+newtype ConvergenceLimit = ConvergenceLimit Float deriving(Show)
+newtype Path = FilePath String deriving(Show)
+
+data Token = Limit String | Path String | HELP
 
 data Argument = Argument ColorLimit ConvergenceLimit Path deriving(Show)
-data Token = Limit Int | Path String | HELP
 
+newArgument :: ColorLimit -> ConvergenceLimit -> String -> Argument
+newArgument col conv l = Argument col conv (FilePath l)
 
-readLimit :: Maybe Int -> Token
-readLimit (Just a) | a > 0     = Limit a
-                   | otherwise = throw $ BadArgument "Limits must be positive."
-readLimit Nothing              = throw $ BadArgument "Limits must be integer."
+readConvergenceLimit :: Maybe Float -> ConvergenceLimit
+readConvergenceLimit (Just a) | a > 0     = ConvergenceLimit a
+                              | otherwise = throw $ BadArgument "Convergence limit must be positive."
+readConvergenceLimitNothing               = throw $ BadArgument "Convergence limit must be integer."
+
+readColorLimit :: Maybe Int -> ColorLimit
+readColorLimit (Just a) | a > 0     = ColorLimit a
+                        | otherwise = throw $ BadArgument "Color limit must be positive."
+readColorLimit Nothing              = throw $ BadArgument "Color limit must be integer."
 
 lexer :: [String] -> [Token]
 lexer []        = []
 lexer ("-h":xs) = [HELP]
 lexer [l]       = [Path l]
-lexer (l:xs)    = readLimit (readMaybe l) : lexer xs
+lexer (l:xs)    = Limit l : lexer xs
 
 parser :: [Token] -> Argument
 parser (HELP:_)                        = throw SendHelp
-parser [Limit col, Limit conv, Path l] = Argument col conv l
+parser [Limit col, Limit conv, Path l] = newArgument (readColorLimit $ readMaybe col) (readConvergenceLimit $ readMaybe conv) l
 parser _                               = throw $ BadArgument "Bad usage, retry with -h."
 
 getColorLimit :: Argument -> ColorLimit
@@ -45,6 +53,5 @@ getColorLimit (Argument col _ _) = col
 getConvergenceLimit :: Argument -> ConvergenceLimit
 getConvergenceLimit (Argument _ conv _) = conv
 
-getPath :: Argument -> Path
-getPath (Argument _ _ path) = path
-
+getPath :: Argument -> String
+getPath (Argument _ _ (FilePath path)) = path
