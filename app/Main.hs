06c76b8bac9b87f0a2783b22e5e71db0e3f4ade6
Lilian VERLHAC <lilian.verlhac@epitech.eu>
2020-06-23 00:24:04 +0200

    [UPDATE] Main
    Now imageCompressor will output a P6 formated PPM picture

--- app/Main.hs
+++ app/Main.hs
@@ -6,18 +6,19 @@ import System.Environment (getArgs)
 --
 
 import ArgumentManager (Argument (..), getPath, parseAndLexArguments)
-import ImageDefinition.Cluster (Cluster, showClusters)
+import ImageDefinition.Cluster (Cluster)
 import ImageDefinition.Pixel (Pixel)
 import Compressor (compressor, initClusters)
 import FileReader (parseFile)
 import Exception (exceptionHandler, ICExceptions(..))
+import Netpbm.PPM.Make (craft, newPpmBlueprint)
 
 main :: IO ()
 main = handle exceptionHandler $
        do argv <- getArgs
           let args = parseAndLexArguments argv
           fileContent <- catch (readFile $ getPath args) (\e -> throw $ BadArgument $ show (e::IOException))
-          mapM_ putStrLn (showClusters . imageCompressor args $ parseFile fileContent)
+          craft $ newPpmBlueprint "toto.ppm" $ imageCompressor args $ parseFile fileContent
 
 imageCompressor :: Argument -> [Pixel] -> [Cluster]
 imageCompressor (Argument col conv _) pix = compressor (initClusters pix col) pix conv
